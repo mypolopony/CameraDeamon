0ff916a7c48ce5dffb2d685a10507d57095580f6
tegra-ubuntu <selwyn@agridata.ai>
2018-02-18 10:52:13 -0800

    Trying to add CUDA

--- test.cpp
+++ test.cpp
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <iostream>
+#include <chrono>
+
+// AgriData
 #include "AGDUtils.h"
 
 // Include files to use openCV
@@ -7,7 +10,47 @@
 #include "opencv2/highgui.hpp"
 #include "opencv2/imgproc.hpp"
 
-int SampleRead()
+// Redis
+#include <redox.hpp>
+
+// Logging
+#include <easylogging++.h>
+
+INITIALIZE_EASYLOGGINGPP
+
+void SampleRedis() {
+    redox::Redox rdx;
+    std::string s;
+    
+    // Connect
+    if (rdx.connect() == 1) {
+        
+        // Single pop from detection
+        /*
+        rdx.command<std::string>({"LPOP", "detection"}, [](redox::Command<std::string>& c) {
+          if(c.ok()) {
+            std::cout << "Hello, async " << c.reply() << std::endl;
+          } else {
+            std::cerr << "Command has error code " << c.status() << std::endl;
+          }
+        });
+         * */
+        
+        // More appropriate looping command repeats until cmd.free()
+        redox::Command<std::string>& cmd = rdx.commandLoop<std::string>({"LPOP", "detection"}, [](redox::Command<std::string>& c) {
+          if(c.ok()) 
+              std::cout << c.cmd() << ": " << c.reply() << std::endl;
+        }, 1);
+
+        std::this_thread::sleep_for(std::chrono::seconds(10));
+        
+        cmd.free();
+        rdx.disconnect();
+    }
+}
+
+
+void SampleRead()
 {
     std::string filename = "/data/output/2018-02-13_16-49/0030531BF616/16_50.hdf5";
 
@@ -25,6 +68,4 @@ int SampleRead()
 
     // Verify the image has been grabbed
     imwrite("test_reader.jpg", n);
-
-    return 0;
 }
