ef64d95a0634a0b36d074022b1a63023b3f045a2
tegra-ubuntu <selwyn@agridata.ai>
2018-04-13 19:53:03 -0700

    Makefiles and CMake

---
+++ CameraDeamon/CMakeFiles/CameraDeamon.dir/DependInfo.cmake
@@ -0,0 +1,40 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/nvidia/CameraDeamon/AGDUtils.cpp" "/home/nvidia/CameraDeamon/CameraDeamon/CMakeFiles/CameraDeamon.dir/AGDUtils.cpp.o"
+  "/home/nvidia/CameraDeamon/AgriDataCamera.cpp" "/home/nvidia/CameraDeamon/CameraDeamon/CMakeFiles/CameraDeamon.dir/AgriDataCamera.cpp.o"
+  "/home/nvidia/CameraDeamon/lib/easylogging++.cc" "/home/nvidia/CameraDeamon/CameraDeamon/CMakeFiles/CameraDeamon.dir/lib/easylogging++.cc.o"
+  "/home/nvidia/CameraDeamon/main.cpp" "/home/nvidia/CameraDeamon/CameraDeamon/CMakeFiles/CameraDeamon.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "OPENCV_TRAITS_ENABLE_DEPRECATED"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  "../lib"
+  "/usr/include/lib"
+  "/opt/pylon5/include"
+  "/usr/local/include/bsoncxx/v_noabi"
+  "/usr/local/include/mongocxx/v_noabi"
+  "/usr/local/include/bsoncxx/v_noabi/bsoncxx"
+  "/usr/include/opencv2"
+  "/data/opencv_contrib/modules/xfeatures2d/include"
+  "/data/CMake-hdf5-1.10.1/HDF_Group/HDF5/1.10.1/include"
+  "/usr/local/include"
+  "/usr/local/include/opencv"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CameraDeamon/CMakeFiles/CameraDeamon.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CameraDeamon.dir/main.cpp.o"
+  "CMakeFiles/CameraDeamon.dir/AgriDataCamera.cpp.o"
+  "CMakeFiles/CameraDeamon.dir/AGDUtils.cpp.o"
+  "CMakeFiles/CameraDeamon.dir/lib/easylogging++.cc.o"
+  "CameraDeamon.pdb"
+  "CameraDeamon"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CameraDeamon.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
