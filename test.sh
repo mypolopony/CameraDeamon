e8d16280ddaf7aaf0ebb0e9f356a0bc905dba288
tegra-ubuntu <selwyn@agridata.ai>
2018-04-16 02:20:44 -0700

    - Removed extraneous historical code - Reinstated proper box / client db query - Final HDF5 implementations - Routine script for easy testing

--- test.sh
+++ test.sh
@@ -1,32 +1,22 @@
 #!/bin/bash
 
-SECS=${1:-9999}
+echo "Making"
 make
-echo "Killing"
-ps aux  |  grep -i EmbeddedServer  |  awk '{print $2}'  |  xargs sudo kill -9
-ps aux  |  grep -i CameraDeamon  |  awk '{print $2}'  |  xargs sudo kill -9
 
-echo "Starting Server"
-/home/nvidia/EmbeddedServer/env/bin/python $HOME/EmbeddedServer/server.py & 
-SERVER=$!
-echo "Starting Camera Deamon"
-$HOME/CameraDeamon/CameraDeamon/Debug/CameraDeamon &
-CAMERA=$!
-echo "Sleeping for 10"
+echo "Restarting"
+sudo systemctl restart camerastack
+
+echo "Waiting 10 seconds"
 sleep 10
-START=1
-END=$SECS
-echo "Initiating scan ($END seconds)"
-http http://localhost:5000/start
-for (( c=$START; c<=$END; c++ ))
-do	
-	echo "$c of $END" 
-	# http http://localhost:5000/live-data
-    sleep 1
-done 
 
-echo "Killing"
-http http://localhost:5000/stop
-sleep 6
-kill $SERVER
-kill $CAMERA
+echo "Disable annoying IMU"
+sudo systemctl stop imu
+
+echo "Recording"
+http http://localhost/start
+
+echo ". . . for 10"
+sleep 10
+
+echo "Stopping"
+http http://localhost/stop
